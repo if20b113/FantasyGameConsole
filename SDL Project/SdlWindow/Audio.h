bebbc88873fd7ea52154025702423deb437a920d
Mohamed Makled <makled1008@gmail.com>
2022-11-17 21:39:27 +0100

    added playing soundtrack when starting

---
+++ SDL Project/SdlWindow/Audio.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <SDL.h>
+
+class Audio {
+public:
+    ~Audio();
+    void load(const char* filename);
+    void play();
+private:
+    SDL_AudioSpec wavSpec;
+    Uint32 wavLength;
+    Uint8* wavBuffer;
+    SDL_AudioDeviceID deviceId;
+};
+
+#endif //AUDIO_H
--- SDL Project/SdlWindow/main.cpp
+++ SDL Project/SdlWindow/main.cpp
@@ -1,16 +1,57 @@
 #include <SDL.h>
 #include "DEBUGGER.h"
+#include "Audio.h"
+
+bool isRunning;
+void handleEvents();
+
+
 int main(int argc, char* argv[])
 {
+
+	Audio examplesound;
+
+
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("Hello world!", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetWindowMinimumSize(window, 100, 100);
 
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
-	SDL_Delay(3000);
+	isRunning = true;
+
+	while (isRunning) {
+		examplesound.load("C:/InnoLab SDL/BasicSDL/BasicSDL/PinkPanther60.wav");
+		examplesound.play();
+		handleEvents();
+		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+		SDL_RenderClear(renderer);
+		SDL_RenderPresent(renderer);
+	}
+
+
+
 	DEBUGGER dbg = DEBUGGER::Instance();
 	dbg.log("Example log");
 	return 0;
+}
+
+
+
+void handleEvents() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+
+	switch (event.type) {
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+	default:
+		break;
+	}
 }
