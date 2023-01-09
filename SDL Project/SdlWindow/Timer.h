6fffaa92eb74dcab2a23cf790ffebd9a371fcc9c
if20b099 <if20b099@technikum-wien.at>
2023-01-09 20:24:58 +0100

    Fixed Timer. Added commented out example code
    Fixed Timer. Added commented out example code. You can copy paste the code into the main function to see it working.

--- SDL Project/SdlWindow/Timer.h
+++ SDL Project/SdlWindow/Timer.h
@@ -8,10 +8,11 @@
 #include <Windows.h>
 #include <SDL_ttf.h>
 #include <string>
+#include "globals.h"
+
 
 class Timer
 {
-
 	/*
 		Timer has 2 tasks:
 		Keep an internal timer (clock) to measure the entire time
@@ -22,64 +23,13 @@ class Timer
 private:
 	bool bIsStarted;
 	clock_t timeClock;
-	SDL_Renderer* renderer;
+	WindowStorage* storage;
 	std::thread* userTimerThread = NULL;
-
-	SDL_Window* window = NULL;
-	SDL_Texture* texture = NULL, *text = NULL;
-	TTF_Font* font;
-	SDL_Renderer* timerRend = NULL;
 
 	bool doTimer()
 	{
-		// https://thenumb.at/cpp-course/sdl2/06/06.html
-		if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-			std::cout << "Error initializing SDL: " << SDL_GetError() << std::endl;
-			return false;
-		}
-		// Initialize SDL_ttf
-		if (TTF_Init() < 0) 
-		{
-			std::cout << "Error intializing SDL_ttf: " << TTF_GetError() << std::endl;
-			return false;
-		}
-		SDL_SetRenderDrawColor(timerRend, 255, 255, 255, 255);
-		SDL_RenderClear(timerRend);
-		window = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
-		if (!window) {
-			std::cout << "Error creating window: " << SDL_GetError() << std::endl;
-			return false;
-		}
-
-		timerRend = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-		if (!timerRend) {
-			std::cout << "Error creating renderer: " << SDL_GetError() << std::endl;
-			return false;
-		}
-
-		// Load font
-		font = TTF_OpenFont("resources/tahoma.ttf", 20);
-		if (!font) {
-			std::cout << "Error loading font: " << TTF_GetError() << std::endl;
-			return false;
-		}
-		
-
-		SDL_Rect dest;
-		SDL_Color foreground = { 0, 0, 0 };
-		std::string timeStr = "00:00";
-		SDL_Surface* text_surf = TTF_RenderText_Solid(font, timeStr.c_str(), foreground);
-		text = SDL_CreateTextureFromSurface(timerRend, text_surf);
-
-		dest.x = 320 - (text_surf->w / 2.0f);
-		dest.y = 240;
-		dest.w = text_surf->w;
-		dest.h = text_surf->h;
-		SDL_RenderCopy(timerRend, text, NULL, &dest);
-
-		SDL_DestroyTexture(text);
-		SDL_FreeSurface(text_surf);
-
+		WindowManager manager;
+		std::string timeStr = "";
 		int minutes = 0;
 		int seconds = 0;
 		
@@ -95,9 +45,7 @@ private:
 			}
 			adjustTimer(minutes, seconds, &timeStr);
 			// Clear old timer
-			text_surf = TTF_RenderText_Solid(font, timeStr.c_str() , foreground);
-			text = SDL_CreateTextureFromSurface(timerRend, text_surf);
-			SDL_RenderPresent(timerRend);
+			manager.displayText(0,0,timeStr,*storage);
 			if (minutes >= 2)
 			{
 				run = false;
@@ -132,27 +80,11 @@ private:
 			time->append(std::to_string(seconds));
 		}
 	}
-	void initiateUserTimer()
-	{
-		//bIsStarted = true;
-		//std::thread Thread(doTimer);
-		//userTimerThread = &Thread;
-	}
 public:
-	Timer(SDL_Renderer* rend) 
+	Timer(WindowStorage* store) 
 	{
+		storage = store;
 		bIsStarted = false;
-		if (rend == NULL)
-		{
-			std::cout << "critical error: passed in renderer for Timer is NULL.\n";
-			renderer = NULL;
-		}
-		else
-		{
-			timeClock = clock();
-			renderer = rend;
-			//initiateUserTimer();
-		}
 	};
 
 	void use()
@@ -162,18 +94,7 @@ public:
 	}
 	~Timer()
 	{
-		//userTimerThread->join();
-		TTF_CloseFont(font);
-		SDL_DestroyTexture(texture);
-		texture = NULL;
-
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
-		window = NULL;
-		renderer = NULL;
 		std::cout << "Lifespan of Timer in ms: " << endClock() << std::endl;
-		TTF_Quit();
-		SDL_Quit();
 	}
 	
 	float endClock()
@@ -182,6 +103,15 @@ public:
 		return static_cast<float>((timeClock = clock() - timeClock))/CLOCKS_PER_SEC;
 	}
 
+	/* Example usage (copy paste in main):
+	
+		WindowManager manager;
+		const char* winName = "MyWindow";
+		WindowStorage* store = manager.getWindow(manager.createWindow(winName), PER_PTR);
+		manager.RenderWindow(store->winRend);
+		Timer time(store);
+		time.use();
+	*/
 	
 
 };
