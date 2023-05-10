cd6597b766b1dfed2cf8da6734e9432d413f4370
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-05-10 15:16:43 +0200

    added sdl image lib

--- ProjectFantasyGameConsole/EXPORT_for_win.bat
+++ ProjectFantasyGameConsole/EXPORT_for_win.bat
@@ -4,3 +4,4 @@ copy build\game\Debug\FantasyGame.exe %TARGET%
 copy build\engine\Debug\FantasyGameEngine.dll %TARGET%
 copy external_deps\sdl2\SDL2.dll %TARGET%
 copy external_deps\sdl2_ttf\SDL2_ttf.dll %TARGET%
+copy external_deps\sdl2_image\SDL2_image.dll %TARGET%
