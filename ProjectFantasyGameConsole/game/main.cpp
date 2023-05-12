f5a2637b506c522e2ff084e9482d7556dfca7543
adam-pikusa <114311367+adam-pikusa@users.noreply.github.com>
2023-05-12 12:01:23 +0200

    added filemanager

--- ProjectFantasyGameConsole/game/main.cpp
+++ ProjectFantasyGameConsole/game/main.cpp
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <iostream>
 
 #include "FantasyGameEngine/FantasyGameEngine.hpp"
+#include "FantasyGameEngine/FileManager.hpp"
 
 int main()
 {
 	FantasyGameEngine::test_lua();
 	FantasyGameEngine::test_sdl();
+
+	puts("reading file ...");
+	auto test_file = FantasyGameEngine::read_text_from_file("assets/scripts/test.lua");
+	std::cout << test_file << std::endl;
+
+	FantasyGameEngine::write_text_to_file("test.txt", test_file);
 }
